30e9131b1a88ffb1f19884c6064116f0bdbe5d5b
yangj96 <yangj_96@126.com>
2019-10-16 21:54:06 +0800

    backtrace 10/16/2019

---
+++ 17.letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=17 lang=cpp
+ *
+ * [17] Letter Combinations of a Phone Number
+ *
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (43.08%)
+ * Likes:    2590
+ * Dislikes: 337
+ * Total Accepted:    449.6K
+ * Total Submissions: 1M
+ * Testcase Example:  '"23"'
+ *
+ * Given a string containing digits from 2-9 inclusive, return all possible
+ * letter combinations that the number could represent.
+ * 
+ * A mapping of digit to letters (just like on the telephone buttons) is given
+ * below. Note that 1 does not map to any letters.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 
+ * 
+ * Note:
+ * 
+ * Although the above answer is in lexicographical order, your answer could be
+ * in any order you want.
+ * 
+ */
+class Solution {
+public:
+    string map[9] = {"", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.empty()) return res;
+        res.push_back("");
+        for (auto c : digits) {
+            vector<string> path;
+            for (auto u : map[c - '1']) {
+                for (auto now : res) {
+                    path.push_back(now + u);
+                }
+            }
+            res = path;
+        }
+        return res;
+    }
+};
+
